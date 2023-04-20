4b417324d71d928c81e8b277e16e98e21815ba85
Kulonbekov <kulonbekov@gmail.com>
2023-04-20 20:08:47 +0600

    1) из файла application.properties, закомментировал три строки кода (напиши потом для чего они нужны, когда они активные, у меня таблицы не создаются в БД)
    2. Создал папку controllers, туа добавил класс AuthenticationController

---
+++ src/main/java/com/example/habittracker/config/controllers/AuthenticationController.java
@@ -0,0 +1,58 @@
+package com.example.habittracker.config.controllers;
+
+import com.example.habittracker.models.dtos.UserDto;
+import com.example.habittracker.models.entities.User;
+import com.example.habittracker.security.jwt.JwtTokenProvider;
+import com.example.habittracker.security.securityDto.AuthenticationRequestDto;
+import com.example.habittracker.security.securityDto.AuthenticationResponseDto;
+import com.example.habittracker.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequestDto requestDto) {
+        try {
+            String username = requestDto.getUsername();
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, requestDto.getPassword()));
+            User user = userService.findByUsername(username);
+
+            String token = jwtTokenProvider.createToken(username, user.getRoles());
+
+            AuthenticationResponseDto responseDto = new AuthenticationResponseDto();
+            responseDto.setUsername(username);
+            responseDto.setToken(token);
+
+            return ResponseEntity.ok(responseDto);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.NOT_FOUND);
+
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserDto userDto) {
+        try {
+            return ResponseEntity.ok(userService.register(userDto));
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Invalid save User: " + userDto.getUsername() + " ");
+        }
+    }
+}
