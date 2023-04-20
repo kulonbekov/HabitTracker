606e5acca685d7244b672effb810137ff9bf86f0
Kulonbekov <kulonbekov@gmail.com>
2023-04-20 19:36:20 +0600

    1) Настроил application.properties 2) настроил pom.xml (добавил нужные зависимости) 3) поменял названия БД (bd_habit_tracker) 4) пароль пока мой стоит 5) server.port = 8075

--- src/main/java/com/example/habittracker/repository/UserRep.java
+++ src/main/java/com/example/habittracker/repository/UserRep.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRep extends JpaRepository<User, Long> {
+
+    User findByUsername(String name);
+    User findByEmail(String email);
 }
