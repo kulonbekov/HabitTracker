bf7a26b4a46bbe05211364bcf0fd8647f893e020
Admin <abdutokt2004>
2023-04-20 17:06:04 +0600

    Initial commit

---
+++ src/main/java/com/example/habittracker/entity/Habit.java
@@ -0,0 +1,33 @@
+package com.example.habittracker.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = Habit.TABLE_NAME)
+public class Habit {
+
+    public static final String TABLE_NAME = "HABITS";
+    public static final String SEQ_NAME = TABLE_NAME + "_SEQ";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = SEQ_NAME)
+    @SequenceGenerator(name = SEQ_NAME, sequenceName = SEQ_NAME, allocationSize = 1)
+    private Long id;
+
+    @Column(name = "name_habit")
+    private String nameHabit;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "count_in_day")
+    private Integer countInDay;
+}
