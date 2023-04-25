62faf1532d64d092917d933a4bcedc9f878887f1
Kulonbekov <kulonbekov@gmail.com>
2023-04-25 20:33:05 +0600

    1) Добавил CRUD для AchievementController.class

--- src/main/java/com/example/habittracker/controllers/AchievementController.java
+++ src/main/java/com/example/habittracker/controllers/AchievementController.java
@@ -1,6 +1,6 @@
 package com.example.habittracker.controllers;
 
-import com.example.habittracker.models.dtos.HabitDto;
+import com.example.habittracker.models.dtos.AchievementDto;
 import com.example.habittracker.services.AchievementService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Api(tags = "5. Достижения")
 @RestController
@@ -24,6 +26,31 @@ public class AchievementController {
             return new ResponseEntity<>(achService.save(profileId, habitId), HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+    @GetMapping("/find/by/id")
+    @ApiOperation("Поиск достижения по id")
+    ResponseEntity<?> findById(@RequestParam Long id) {
+        try {
+            return new ResponseEntity<>(achService.findById(id), HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("/find/all")
+    @ApiOperation("Вывод всех привычек")
+    ResponseEntity<List<AchievementDto>> findAll() {
+        return ResponseEntity.ok(achService.findAll());
+    }
+
+    @DeleteMapping("/delete")
+    @ApiOperation("Удаления достижения")
+    ResponseEntity<?> delete(@RequestParam Long id) {
+        try {
+            achService.delete(id);
+            return ResponseEntity.ok("database entry deleted");
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
